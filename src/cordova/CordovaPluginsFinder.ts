6f826c4b8f88e31a61ccdd5cdd69b651058d9a5e
Marco Bonati <marco.bonati@vipera.com>
2017-07-06 11:56:17 +0200

    added community plugins search

---
+++ src/cordova/CordovaPluginsFinder.ts
@@ -0,0 +1,95 @@
+'use babel'
+
+/*!
+ * Dynamic Engine Workbench
+ * Copyright(c) 2017 Dynamic Engine Team @ Vipera Plc
+ * MIT Licensed
+ */
+
+import { Logger } from '../logger/Logger'
+
+const _ = require("lodash");
+const $ = require("jquery");
+const http = require("http");
+const httpReq = require("httpReq");
+
+/**
+ * This class allows to search plugins from the community repository
+ */
+export class CordovaPluginsFinder {
+
+  protected pluginsData:any = {};
+
+    constructor(){
+      Logger.getInstance().debug("Creating Cordova Plugins Finder...");
+    }
+
+    /**
+     * Build the query
+     */
+    private buildQueryParamsString(names,keywords,platforms):string{
+      var ret = "";
+      var i = 0;
+      if (keywords && keywords.length>0){
+        for (i=0;i<keywords.length;i++){
+          if (ret.length>0){
+            ret += "+OR+";
+          }
+          ret += "keywords:" + keywords[i];
+        }
+      }
+
+      //the platforms are keywords
+      if (platforms && platforms.length>0){
+        for (i=0;i<platforms.length;i++){
+          if (ret.length>0){
+            ret += "+OR+";
+          }
+          ret += "keywords:" + platforms[i];
+        }
+      }
+
+      if (names && names.length>0){
+        for (i=0;i<names.length;i++){
+          if (ret.length>0){
+            ret += "+OR+";
+          }
+          ret += "name:*" + names[i] +"*";
+        }
+      }
+
+      //Limit to Cordova
+      if (ret.length>0){
+        ret += "+AND+";
+      }
+      ret += 'keywords:"ecosystem:cordova"';
+
+      if (ret.length>0){
+        ret = "&q=" + ret;
+      }
+      return ret;
+    }
+
+    /**
+     * Search plugins from community
+     */
+    public search(names,keywords,platforms):Promise<any>{
+      return new Promise((resolve, reject) => {
+        var baseQueryUrl = 'http://npmsearch.com/query?fields=name,keywords,license,description,author,modified,homepage,version,rating&sort=rating:desc&size=500&start=0';
+        var queryParams = this.buildQueryParamsString(names, keywords, platforms);
+        Logger.getInstance().debug("Query:" + queryParams);
+        var queryUrl = baseQueryUrl + queryParams; //'q=keywords:camera+AND+author:neuber';
+        Logger.getInstance().debug("QueryURL:" + queryUrl);
+        httpReq(queryUrl, function (error, response, body) {
+            if (error){
+              Logger.getInstance().error("Query error: ", error);
+              reject(error);
+            } else {
+              Logger.getInstance().error("Query OK");
+              resolve(JSON.parse(body));
+            }
+        });
+      });
+    }
+
+}
