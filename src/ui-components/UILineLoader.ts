5dc0e2cdb7e9973545be914a728390d62725816a
Enrico <enrico.mazzucchelli@vipera.com>
2017-07-31 18:20:06 +0200

    Create line loader component

--- src/ui-components/UILineLoader.ts
+++ src/ui-components/UILineLoader.ts
@@ -1,9 +1,29 @@
 'use babel'
+import {
+  createText,
+  createElement,
+  insertElement,
+  createIcon
+} from '../element/index';
 import { UIBaseComponent } from './UIComponent'
 export default class UILineLoader extends UIBaseComponent{
+  private onLoading:boolean =false;
   constructor(){
     super();
+    this.initUI();
   }
   initUI(){
+    this.mainElement = createElement('div',{
+      className:'status-loading'
+    });
+  }
+  public setOnLoading(value:boolean){
+    if(this.onLoading != value){
+      this.onLoading = value;
+      this.updateUI();
+    }
+  }
+  private updateUI(){
+    this.mainElement.classList[this.onLoading ? 'add' : 'remove']('active')
   }
 }
