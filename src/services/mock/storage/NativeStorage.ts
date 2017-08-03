3f23152fffd465bda77ff6c2c986f359237d76ff
Enrico <enrico.mazzucchelli@vipera.com>
2017-08-03 16:32:43 +0200

    implements native storage

--- src/services/mock/storage/NativeStorage.ts
+++ src/services/mock/storage/NativeStorage.ts
@@ -1,4 +1,6 @@
 'use babel'
+import {InitOptions} from 'node-persist'
+import {initSync, getItem, setItem, removeItem, clear} from 'node-persist'
 export interface NativeStorage{
   getItem(key:string,success:Function,fail:Function):void
   setItem(key:string,value,success:Function,fail:Function):void
@@ -12,22 +14,53 @@ export class NativeStorageConfig{
 
 class NativeStorageImp implements NativeStorage{
 
-  getItem(key:string,success:Function,fail:Function):void{
+  constructor(conf:NativeStorageConfig){
+    initSync({
+        dir: conf.dbPath,
+        stringify: JSON.stringify,
+        parse: JSON.parse,
+        encoding: 'utf8',
+        logging: false,  // can also be custom logging function
+        continuous: true, // continously persist to disk
+        interval: false, // milliseconds, persist to disk on an interval
+        ttl: false, // ttl* [NEW], can be true for 24h default or a number in MILLISECONDS
+    });
+  }
 
+  getItem(key:string,success:Function,fail:Function):void{
+    getItem(key).then((value) => {
+          if(value){
+            success(value)
+          }else{
+            success(null)
+          }
+        },(err) => {
+          console.error(err);
+          success(null);
+      });
   }
   setItem(key:string,value,success:Function,fail:Function):void{
 
   }
   deleteItem(key:string,success:Function,fail:Function):void{
-
+    removeItem(key).then(() => {
+      success();
+    }, (err) => {
+      console.error(err);
+      fail(err);
+    })
   }
   clear(success:Function,fail:Function):void{
-
+    clear().then(() => {
+      success();
+    }, (err) => {
+      fail(err)
+    });
   }
 }
 
 export class NativeStorageFactory {
   getNativeStorage(config:NativeStorageConfig):NativeStorage{
-    return null;
+    return new NativeStorageImp(config);
   }
 }
