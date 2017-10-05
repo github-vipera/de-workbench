9ef5063644694be7b9640b3ea71c4cf083a0a0c2
Marco Bonati <marco.bonati@vipera.com>
2017-10-05 12:52:40 +0200

    Fixed log varargs

--- src/logger/Logger.ts
+++ src/logger/Logger.ts
@@ -43,14 +43,7 @@ export class Logger {
             maxsize: 5242880, //5MB
             maxFiles: 5,
             colorize: false
-        })
-        /**,
-        new winston.transports.Console({
-            level: 'verbose',
-            handleExceptions: true,
-            json: false,
-            colorize: true
-        })**/
+        }) 
       ]
     });
     this.evtSupport = new EventSupport();
